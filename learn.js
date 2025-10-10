8d2e7f806054befe822e8a79c1ead9bc780752c2
s-sivaselvan <sivaselvan1729@gmail.com>
2025-10-10 16:01:40 +0530

    Upto Rock Paper Scissor Game

---
+++ learn.js
@@ -0,0 +1,16 @@
+console.log("Javascript is working");
+a=6;
+console.log(a);
+let object1={age:33};
+let object2=object1;
+object1.age=22;
+console.log(object2.age);
+console.log(9^5);
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(9,5));
+const sum=(a,b,c)=>{
+    return a+b+c;
+}
+console.log(sum(1,2,3));
